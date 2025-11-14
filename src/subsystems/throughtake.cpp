16beada15fc0fd1c7a60e5b6c014921801eff85c
miyonifluff-sys <miyonifluff@gmail.com>
2025-11-13 18:13:58 -0800

    added Throughtake functionality
    added Throughtake functionality

---
+++ src/subsystems/throughtake.cpp
@@ -0,0 +1,14 @@
+#include "subsystems/throughtake.h"
+
+void ThroughTake::setPercent(double out)
+{
+    throughTakeMotor.spin(vex::forward, (out * MAX_RPM), vex::rpm);
+}
+void ThroughTake::intake()
+{
+    setPercent(INTAKE_SPEED);
+}
+void ThroughTake::reverse()
+{
+    setPercent(REVERSE_SPEED);
+}
