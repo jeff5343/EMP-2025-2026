e8bf3a09f41389b0fa17cc3046ae3ee4846f39d8
miyonifluff-sys <miyonifluff@gmail.com>
2025-11-14 17:08:14 -0800

    added all the function declartions for superstructure class

---
+++ include/superstructure.h
@@ -0,0 +1,31 @@
+#include "vex.h"
+#include "subsystems/drivetrain.h"
+#include "subsystems/intake.h"
+#include "subsystems/outtake.h"
+#include "subsystems/throughtake.h"
+
+#include "subsystems/arm.h"
+#include "subsystems/flap.h"
+#include "subsystems/poop_chute.h"
+
+class Superstructure
+{
+private:
+    // subsystems
+    Drivetrain drivetrain{};
+    InTake intake{};
+    OutTake outtake{};
+    ThroughTake throughtake{};
+    Arm arm{};
+    Flap flap{};
+    PoopChute poopChute{};
+
+public:
+    void intake();
+    void poop();
+    void bottomScoring();
+    void raisedScoreingPosition();
+    void levelScoringPosition();
+    void armScoringOut();
+    void travelPosition();
+}
