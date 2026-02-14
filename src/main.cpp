97cd87d524ff6029ef2f77b697711609931a7534
miyonifluff-sys <miyonifluff@gmail.com>
2026-02-14 12:00:15 -0800

    changed to Blue Left

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ using namespace vex;
 // A global instance of competition
 competition Competition;
 Robot robot;
-ALLIANCE alliance = ALLIANCE::RED_RIGHT;
+ALLIANCE alliance = ALLIANCE::BLUE_LEFT;
 
 // define your global instances of motors and other devices here
 
